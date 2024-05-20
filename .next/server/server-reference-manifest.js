200c8b8e0fdeb0245a29b2c88d42d59cd954e8a3
ibnume <160526908+ibnume@users.noreply.github.com>
2024-05-20 20:30:21 +0700

    aa

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vmv63eCOGDmfBdV/AYcjLhlbTt0dpHvefrQmmsEKttU=\"\n}"
--- src/app/components/SubComponent/AccLogIn.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AccLogIn = () => {
-	return <div></div>;
-};
-
-export default AccLogIn;
